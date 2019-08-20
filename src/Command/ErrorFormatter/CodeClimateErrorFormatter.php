adc3a3b40cca60c83451550327a8284811f2c7a1
Eugeniy Kurasov <eugeniy.kurasov@socialtechnologies.io>
2019-08-20 23:37:02 +0300

    Add CodeClimatFormat for use in gitlab CodeQuality report

---
+++ src/Command/ErrorFormatter/CodeClimateErrorFormatter.php
@@ -0,0 +1,75 @@
+<?php declare(strict_types = 1);
+
+namespace PHPStan\Command\ErrorFormatter;
+
+use Nette\Utils\Json;
+use PHPStan\Command\AnalysisResult;
+use Symfony\Component\Console\Style\OutputStyle;
+
+class CodeClimateErrorFormatter implements ErrorFormatter
+{
+	public function formatErrors(AnalysisResult $analysisResult, OutputStyle $style): int
+	{
+		$errorsArray = [];
+
+		foreach ($analysisResult->getFileSpecificErrors() as $fileSpecificError) {
+			$error = [
+				'type' => 'issue',
+				'check_name' => 'PHPStan',
+				'description' => $fileSpecificError->getMessage(),
+				'categories' => ['Style'],
+				'location' => [
+					'path' => str_replace(
+						rtrim($analysisResult->getCurrentDirectory(), '/') . '/',
+						'',
+						$fileSpecificError->getFile()
+					),
+					'lines' => [
+						'begin' => $fileSpecificError->getLine(),
+					],
+				],
+				'fingerprint' => hash(
+					'sha256',
+					implode(
+						[
+							$fileSpecificError->getFile(),
+							$fileSpecificError->getLine(),
+							$fileSpecificError->getMessage(),
+						]
+					)
+				),
+			];
+
+			if (!$fileSpecificError->canBeIgnored()) {
+				$error['severity'] = 'blocker';
+			}
+
+			$errorsArray[] = $error;
+		}
+
+		foreach ($analysisResult->getNotFileSpecificErrors() as $notFileSpecificError) {
+			$errorsArray[] = [
+				'type' => 'issue',
+				'check_name' => 'PHPStan',
+				'description' => $notFileSpecificError,
+				'categories' => ['Style'],
+				'location' => [
+					'path' => $analysisResult->getCurrentDirectory() . "/.",
+					'positions' => [
+						'offset' => 0,
+					],
+				],
+				'fingerprint' => hash(
+					'sha256',
+					$notFileSpecificError
+				),
+			];
+		}
+
+		$json = Json::encode($errorsArray, Json::PRETTY);
+
+		$style->write($json);
+
+		return $analysisResult->hasErrors() ? 1 : 0;
+	}
+}
